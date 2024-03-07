ea82b62d737c58cad01a7d13a3058728175c9921
jesusvaldes2808 <jesus.valdes@cetus.com.co>
2024-03-07 12:04:08 -0500

    Primer commit

---
+++ src/app/countries/countries.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CountriesRoutingModule } from './countries-routing.module';
+import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    SelectorPageComponent,
+  ],
+  imports: [
+    CommonModule,
+    CountriesRoutingModule,
+    ReactiveFormsModule,
+
+  ],
+  exports: [
+
+  ]
+})
+export class CountriesModule { }
